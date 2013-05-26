4b430b4604d5bcc21ae41ace59ab74764d5880de
iseki <iseki.m.aa@gmail.com>
2013-05-26 11:17:09 +0900

    more effective

--- SRM580Div2/Easy.cpp
+++ SRM580Div2/Easy.cpp
@@ -6,35 +6,14 @@ using namespace std;
 
 class ShoutterDiv2 {
 public:
-	struct state {
-		int start;
-		int end;
-	};
-	struct comp {
-		bool operator()(const state &lhs,const state &rhs)
-		{
-			return lhs.start < rhs.start;
-		}
-	};
-
 	int count(vector <int> s, vector <int> t)
 	{
-		vector<state> info(s.size());
-		state tmp;
-		for (int i=0; i<s.size(); ++i) {
-			tmp.start = s.at(i);
-			tmp.end = t.at(i);
-			info.at(i) = tmp;
-		}
-
-		comp c;
-		sort(info.begin(), info.end(), c);
-
 		int cnt = 0;
-		for (int i=0; i<s.size(); ++i)
-			for (int j=i+1; j<s.size(); ++j)
-				if (info.at(i).end >= info.at(j).start)
-					++cnt;
+		int size = static_cast<int>(s.size());
+		for (int i=0; i<size; ++i)
+			for (int j=i+1; j<size; ++j)
+				if (t[i]<s[j] || t[j]<s[i]);
+				else ++cnt;
 
 		return cnt;
 	}
