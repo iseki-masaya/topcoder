45df69d9d453c4fa2880626c7991096fb755d19c
iseki <iseki.m.aa@gmail.com>
2013-10-06 12:25:56 +0900

    solve

---
+++ Div1/SRM570Div1/Easy.cpp
@@ -0,0 +1,87 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <list>
+#include <algorithm>
+#include <sstream>
+#include <set>
+#include <cmath>
+#include <map>
+#include <stack>
+#include <queue>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <numeric>
+
+#define INF (1<<30)
+#define EPS 1e-6
+
+using namespace std;
+
+const int dx[] = {0,1,0,-1};
+const int dy[] = {1,0,-1,0};
+
+typedef pair<long long, long long> P;
+
+class RobotHerb {
+
+public:
+
+    long long
+    simulate(vector<int> &a,int f)
+    {
+        P p(0,0);
+        int dir = 0;
+        for (int n=0; n<f; ++n) {
+            for (int i=0; i<a.size(); ++i) {
+                p.first += a[i]*dx[dir] , p.second += a[i]*dy[dir];
+                dir = (dir+a[i])%4;
+            }
+        }
+        return abs(p.first) + abs(p.second);
+    }
+
+    long long
+    getdist(int T, vector <int> a)
+    {
+        int f =  accumulate(a.begin(), a.end(), 0)%4;
+        if (f == 0) ++ f;
+        else if (f == 1 || f == 3) f = 4;
+
+        return (T/f) * simulate(a, f) + simulate(a, T%f);
+    }
+
+
+    // BEGIN CUT HERE
+public:
+    void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+private:
+    template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+    void verify_case(int Case, const long long &Expected, const long long &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+    void test_case_0() { int Arg0 = 1; int Arr1[] = {1,2,3}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); long long Arg2 = 2LL; verify_case(0, Arg2, getdist(Arg0, Arg1)); }
+    void test_case_1() { int Arg0 = 100; int Arr1[] = {1}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); long long Arg2 = 0LL; verify_case(1, Arg2, getdist(Arg0, Arg1)); }
+    void test_case_2() { int Arg0 = 5; int Arr1[] = {1,1,2}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); long long Arg2 = 10LL; verify_case(2, Arg2, getdist(Arg0, Arg1)); }
+    void test_case_3() { int Arg0 = 1000000000; int Arr1[] = {100}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); long long Arg2 = 100000000000LL; verify_case(3, Arg2, getdist(Arg0, Arg1)); }
+    void test_case_4() { int Arg0 = 570; int Arr1[] = {2013,2,13,314,271,1414,1732}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); long long Arg2 = 4112LL; verify_case(4, Arg2, getdist(Arg0, Arg1)); }
+
+    // END CUT HERE
+
+
+};
+
+
+
+// BEGIN CUT HERE
+
+int main() {
+
+    RobotHerb ___test;
+
+    ___test.run_test(-1);
+
+}
+
+// END CUT HERE
