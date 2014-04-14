b0b561b100bf686311b66928f3792dfbd4d9f27e
iseki <iseki.m.aa@gmail.com>
2014-04-14 14:16:55 +0900

    solve

---
+++ Div1/SRM600Div1/Easy.cpp
@@ -0,0 +1,103 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <list>
+#include <algorithm>
+#include <sstream>
+#include <set>
+#include <cmath>
+#include <map>
+#include <stack>
+#include <queue>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <numeric>
+#include <bitset>
+#include <deque>
+
+const long long LINF = (1e15);
+const int INF = (1<<27);
+#define EPS 1e-6
+const int MOD = 1000000007;
+
+using namespace std;
+
+class ORSolitaire {
+
+public:
+
+    bool is_include(int v, int goal) {
+        for (int i=0; i<32; ++i) {
+            if (v&(1LL<<i) && !(goal&(1LL<<i)) ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    int getMinimum(vector <int> numbers, int goal) {
+        int bits[35];
+        memset(bits, 0, sizeof(bits));
+        int N = (int)numbers.size();
+        for (int i=0; i<N; ++i) {
+            int v = numbers[i];
+            if (!is_include(v, goal))
+                continue;
+
+            int p = 0;
+            while (v > 0) {
+                if (v&1) {
+                    bits[p]++;
+                }
+                v >>= 1;
+                ++p;
+            }
+        }
+
+        int ans = INF;
+        int p = 0;
+        while (goal > 0) {
+            if (goal&1) {
+                ans = min(ans, bits[p]);
+            }
+            goal >>= 1;
+            ++p;
+        }
+
+        return ans;
+    }
+
+
+    // BEGIN CUT HERE
+public:
+    void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+private:
+    template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+    void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+    void test_case_0() { int Arr0[] = {1, 2, 4}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 7; int Arg2 = 1; verify_case(0, Arg2, getMinimum(Arg0, Arg1)); }
+    void test_case_1() { int Arr0[] = {1, 2, 4, 7, 8}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 7; int Arg2 = 2; verify_case(1, Arg2, getMinimum(Arg0, Arg1)); }
+    void test_case_2() { int Arr0[] = {12571295, 2174218, 2015120}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1; int Arg2 = 0; verify_case(2, Arg2, getMinimum(Arg0, Arg1)); }
+    void test_case_3() { int Arr0[] = {5,2,4,52,62,9,8,3,1,11,6}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 11; int Arg2 = 3; verify_case(3, Arg2, getMinimum(Arg0, Arg1)); }
+    void test_case_4() { int Arr0[] = {503, 505, 152, 435, 491, 512, 1023, 355, 510, 500, 502, 255, 63, 508, 509, 511, 60, 250, 254, 346}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 510; int Arg2 = 5; verify_case(4, Arg2, getMinimum(Arg0, Arg1)); }
+
+    // END CUT HERE
+
+
+};
+
+
+
+// BEGIN CUT HERE
+
+int main() {
+
+    ORSolitaire ___test;
+
+    ___test.run_test(-1);
+
+}
+
+// END CUT HERE
