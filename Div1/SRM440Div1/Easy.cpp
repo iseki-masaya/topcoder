99ceeda1e85984b0aa531e1cb90ceafcfb13167d
iseki <iseki.m.aa@gmail.com>
2014-05-16 08:33:09 +0900

    solve

--- Div1/SRM440Div1/Easy.cpp
+++ Div1/SRM440Div1/Easy.cpp
@@ -26,65 +26,65 @@ const int MOD = 1000000007;
 using namespace std;
 
 class IncredibleMachine {
-
+	
 public:
-    double calculateTime(double distance, double velocity, double acceleration) {
-        double low=0,high=1000;
-        for (int i=0; i<100; ++i) {
-            double t = (low+high) / 2.0;
-            double d = velocity*t + 0.5*acceleration*t*t;
-            if (distance > d)
-                low = t;
-            else
-                high = t;
-        }
-        return low;
-    }
-
-    double simulate(double g, vector <int> &x, vector <int> &y) {
-        int N = (int)x.size();
-
-        double time = 0;
-        double v = 0;
-        for (int i=1; i<N; ++i) {
-            int dx = x[i] - x[i-1];
-            int dy = y[i-1] - y[i];
-            double r = sqrt(dx*dx + dy*dy);
-            double a = g * dy / r;
-            double t = calculateTime(r, v, a);
-            time += t;
-            v = v + a*t;
-        }
-        return time;
-    }
-
-    double gravitationalAcceleration(vector <int> x, vector <int> y, int T) {
-        double low=0,high=1000;
-        for (int i=0; i<100; ++i) {
-            double g = (low + high) / 2.0;
-            double time = simulate(g, x, y);
-            if (T > time)
-                high = g;
-            else
-                low = g;
-        }
-        return low;
-    }
-
-
-    // BEGIN CUT HERE
+	double calculateTime(double distance, double velocity, double acceleration) {
+		double low=0,high=1e10;
+		for (int i=0; i<100; ++i) {
+			double t = (low+high) / 2.0;
+			double d = velocity*t + 0.5*acceleration*t*t;
+			if (distance > d)
+				low = t;
+			else
+				high = t;
+		}
+		return low;
+	}
+	
+	double simulate(double g, vector <int> &x, vector <int> &y) {
+		int N = (int)x.size();
+		
+		double time = 0;
+		double v = 0;
+		for (int i=1; i<N; ++i) {
+			int dx = x[i] - x[i-1];
+			int dy = y[i-1] - y[i];
+			double r = sqrt(dx*dx + dy*dy);
+			double a = g * dy / r;
+			double t = calculateTime(r, v, a);
+			time += t;
+			v = v + a*t;
+		}
+		return time;
+	}
+	
+	double gravitationalAcceleration(vector <int> x, vector <int> y, int T) {
+		double low=0,high=1e10;
+		for (int i=0; i<100; ++i) {
+			double g = (low + high) / 2.0;
+			double time = simulate(g, x, y);
+			if (T > time)
+				high = g;
+			else
+				low = g;
+		}
+		return low;
+	}
+	
+	
+	// BEGIN CUT HERE
 public:
-    void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); }
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); }
 private:
-    template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
-    void verify_case(int Case, const double &Expected, const double &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
-    void test_case_0() { int Arr0[] = {0,6}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {100,22}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 4; double Arg3 = 9.807692307692307; verify_case(0, Arg3, gravitationalAcceleration(Arg0, Arg1, Arg2)); }
-    void test_case_1() { int Arr0[] = {0,26,100}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {50,26,24}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 4; double Arg3 = 26.743031720603582; verify_case(1, Arg3, gravitationalAcceleration(Arg0, Arg1, Arg2)); }
-    void test_case_2() { int Arr0[] = {0,7,8}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {10,6,0}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 7; double Arg3 = 1.1076837407708007; verify_case(2, Arg3, gravitationalAcceleration(Arg0, Arg1, Arg2)); }
-
-    // END CUT HERE
-
-
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const double &Expected, const double &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arr0[] = {0,6}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {100,22}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 4; double Arg3 = 9.807692307692307; verify_case(0, Arg3, gravitationalAcceleration(Arg0, Arg1, Arg2)); }
+	void test_case_1() { int Arr0[] = {0,26,100}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {50,26,24}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 4; double Arg3 = 26.743031720603582; verify_case(1, Arg3, gravitationalAcceleration(Arg0, Arg1, Arg2)); }
+	void test_case_2() { int Arr0[] = {0,7,8}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {10,6,0}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 7; double Arg3 = 1.1076837407708007; verify_case(2, Arg3, gravitationalAcceleration(Arg0, Arg1, Arg2)); }
+	
+	// END CUT HERE
+	
+	
 };
 
 
@@ -92,11 +92,11 @@ private:
 // BEGIN CUT HERE
 
 int main() {
-
-    IncredibleMachine ___test;
-
-    ___test.run_test(-1);
-
+	
+	IncredibleMachine ___test;
+	
+	___test.run_test(-1);
+	
 }
 
 // END CUT HERE
