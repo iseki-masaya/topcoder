7c7b811535d0efc03ede8bc77b43720f31853780
iseki <iseki.m.aa@gmail.com>
2014-10-24 12:24:53 +0900

    solve

---
+++ Div1/SRM630Div1/Easy.cpp
@@ -0,0 +1,103 @@
+#include <vector>
+#include <string>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <list>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <numeric>
+#include <bitset>
+#include <deque>
+#include <random>
+#include <stdlib.h>
+
+const long long LINF = (1e18);
+const int INF = (1<<28);
+const int sINF = (1<<23);
+const int MOD = 1000000007;
+const double EPS = 1e-6;
+
+using namespace std;
+
+class Egalitarianism3 {
+
+public:
+
+    int maxCities(int n, vector <int> &a, vector <int> &b, vector <int> len) {
+        vector<vector<int> > d(n, vector<int>(n, sINF));
+        for (int i=0; i<a.size(); ++i) {
+            d[a[i]-1][b[i]-1] = len[i];
+            d[b[i]-1][a[i]-1] = len[i];
+        }
+
+        for (int k=0; k<n; ++k) {
+            for (int i=0; i<n; ++i) {
+                for (int j=0; j<n; ++j) {
+                    d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+                }
+            }
+        }
+
+        int res = ( n>1 ? 2 : 1);
+
+        for (int x = 0; x<n; ++x) {
+            map<int, vector<int> > ds;
+            for (int y=0; y<n; ++y) {
+                if (y != x)
+                    ds[ d[x][y] ].push_back(y);
+            }
+
+            for (auto p: ds) {
+                vector<int> added;
+                for (int y: p.second) {
+                    bool good = true;
+                    for (int z: added) {
+                        good = good && (d[y][z] == 2*d[x][y]);
+                    }
+                    if (good) {
+                        added.push_back(y);
+                    }
+                }
+                res = max<int>(res, (int)added.size());
+            }
+        }
+
+        return res;
+    }
+
+    // BEGIN CUT HERE
+public:
+    void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+private:
+    template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+    void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+    void test_case_0() { int Arg0 = 4; int Arr1[] = {1,1,1}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {2,3,4}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); int Arr3[] = {1,1,1}; vector <int> Arg3(Arr3, Arr3 + (sizeof(Arr3) / sizeof(Arr3[0]))); int Arg4 = 3; verify_case(0, Arg4, maxCities(Arg0, Arg1, Arg2, Arg3)); }
+    void test_case_1() { int Arg0 = 6; int Arr1[] = {1,2,3,2,3}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {2,3,4,5,6}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); int Arr3[] = {2,1,3,2,3}; vector <int> Arg3(Arr3, Arr3 + (sizeof(Arr3) / sizeof(Arr3[0]))); int Arg4 = 3; verify_case(1, Arg4, maxCities(Arg0, Arg1, Arg2, Arg3)); }
+    void test_case_2() { int Arg0 = 10; int Arr1[] = {1,1,1,1,1,1,1,1,1}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {2,3,4,5,6,7,8,9,10}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); int Arr3[] = {1000,1000,1000,1000,1000,1000,1000,1000,1000}; vector <int> Arg3(Arr3, Arr3 + (sizeof(Arr3) / sizeof(Arr3[0]))); int Arg4 = 9; verify_case(2, Arg4, maxCities(Arg0, Arg1, Arg2, Arg3)); }
+    void test_case_3() { int Arg0 = 1; int Arr1[] = {}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); int Arr3[] = {}; vector <int> Arg3(Arr3, Arr3 + (sizeof(Arr3) / sizeof(Arr3[0]))); int Arg4 = 1; verify_case(3, Arg4, maxCities(Arg0, Arg1, Arg2, Arg3)); }
+    void test_case_4() { int Arg0 = 2; int Arr1[] = {1}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {2}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); int Arr3[] = {3}; vector <int> Arg3(Arr3, Arr3 + (sizeof(Arr3) / sizeof(Arr3[0]))); int Arg4 = 2; verify_case(4, Arg4, maxCities(Arg0, Arg1, Arg2, Arg3)); }
+
+
+    // END CUT HERE
+
+
+};
+
+
+
+// BEGIN CUT HERE
+
+int main() {
+
+    Egalitarianism3 ___test;
+
+    ___test.run_test(-1);
+
+}
+
+// END CUT HERE
