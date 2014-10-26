c67ae978c98232824f4e682a8e196177732db94e
iseki <iseki.m.aa@gmail.com>
2014-10-26 10:41:27 +0900

    solve

---
+++ Div1/SRM627Div1/Easy.cpp
@@ -0,0 +1,86 @@
+#include <vector>
+#include <string>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <list>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <numeric>
+#include <bitset>
+#include <deque>
+#include <random>
+#include <stdlib.h>
+
+const long long LINF = (1e18);
+const int INF = (1<<28);
+const int sINF = (1<<23);
+const int MOD = 1000000007;
+const double EPS = 1e-6;
+
+using namespace std;
+
+class HappyLetterDiv1 {
+
+public:
+
+    string getHappyLetters(string letters) {
+        map<char, int> c;
+        for (char ch: letters)
+            c[ch]++;
+        string ans = "";
+        for (auto p: c) {
+            priority_queue<int> q;
+            for (auto r: c)
+                if (r != p)
+                    q.push(r.second);
+            while (q.size() > 1) {
+                int v1 = q.top(); q.pop();
+                int v2 = q.top(); q.pop();
+                v1--, v2--;
+                if (v1 != 0)
+                    q.push(v1);
+                if (v2 != 0)
+                    q.push(v2);
+            }
+            if (q.empty() || p.second > q.top())
+                ans += p.first;
+        }
+        return ans;
+    }
+
+
+    // BEGIN CUT HERE
+public:
+    void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+private:
+    template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+    void verify_case(int Case, const string &Expected, const string &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+    void test_case_0() { string Arg0 = "aabbacccc"; string Arg1 = "abc"; verify_case(0, Arg1, getHappyLetters(Arg0)); }
+    void test_case_1() { string Arg0 = "aaaaaaaccdd"; string Arg1 = "a"; verify_case(1, Arg1, getHappyLetters(Arg0)); }
+    void test_case_2() { string Arg0 = "ddabccadb"; string Arg1 = "abcd"; verify_case(2, Arg1, getHappyLetters(Arg0)); }
+    void test_case_3() { string Arg0 = "aaabbb"; string Arg1 = ""; verify_case(3, Arg1, getHappyLetters(Arg0)); }
+    void test_case_4() { string Arg0 = "rdokcogscosn"; string Arg1 = "cos"; verify_case(4, Arg1, getHappyLetters(Arg0)); }
+
+    // END CUT HERE
+
+
+};
+
+
+
+// BEGIN CUT HERE
+
+int main() {
+
+    HappyLetterDiv1 ___test;
+
+    ___test.run_test(-1);
+
+}
+
+// END CUT HERE
